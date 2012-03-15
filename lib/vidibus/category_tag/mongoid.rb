9bfce9ac5eb0882341bb054cdbe827abb40a9764
Andre Pankratz <andre@webwarelab.com>
2012-03-15 14:33:30 +0100

    Instance methods are deprecated

--- lib/vidibus/category_tag/mongoid.rb
+++ lib/vidibus/category_tag/mongoid.rb
@@ -14,24 +14,22 @@ module Vidibus
         end
       end
 
-      module InstanceMethods
-        def tags
-          (tags_hash || {}).inject({}) do |categories, (key,tags)|
-            categories[key] = tags.join(self.class.tags_separator)
-            categories
-          end
+      def tags
+        (tags_hash || {}).inject({}) do |categories, (key,tags)|
+          categories[key] = tags.join(self.class.tags_separator)
+          categories
         end
+      end
 
-        def tags=(categories)
-          raise Error unless categories.kind_of?(Hash)
+      def tags=(categories)
+        raise Error unless categories.kind_of?(Hash)
 
-          tags_hash = {}
-          categories.each do |category, tags|
-            tags = tags.split(self.class.tags_separator).map(&:strip).reject(&:blank?)
-            tags_hash[category.to_s] = tags unless tags.empty?
-          end
-          self.tags_hash = tags_hash
+        tags_hash = {}
+        categories.each do |category, tags|
+          tags = tags.split(self.class.tags_separator).map(&:strip).reject(&:blank?)
+          tags_hash[category.to_s] = tags unless tags.empty?
         end
+        self.tags_hash = tags_hash
       end
     end
   end
